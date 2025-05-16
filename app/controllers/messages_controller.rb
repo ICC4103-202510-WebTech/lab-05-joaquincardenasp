0fb1886aa6dd947d62be2ab4d27fd94fbda2f01e
joaquincardenasp <jicardenas@miuandes.cl>
2025-05-16 17:00:01 -0400

    lab08

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -8,17 +8,29 @@ class MessagesController < ApplicationController
     end
     def new
         @message = Message.new
-      end
-      
-      def create
-        @message = Message.new(message_params)
-        if @message.save
-          redirect_to @message
+    end
+        
+    def create
+          @message = Message.new(message_params)
+          if @message.save
+            redirect_to @message
+          else
+            render :new
+          end
+    end
+
+    def edit
+        @message = Message.find(params[:id])
+    end
+    
+    def update
+        @message = Message.find(params[:id])
+        if @message.update(message_params)
+            redirect_to @message
         else
-          render :new
+            render :edit
         end
-      end
-      
+    end
       private
       
       def message_params
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,6 @@ class User < ApplicationRecord
       end
     
       validates :email, presence: true, uniqueness: true
+      validates :first_name, presence: true
+      validates :last_name, presence: true
 end
